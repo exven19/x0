65255cbe26bffb7333c74c7737e4c5a17cc6e044
Nayeem <exven19@gmail.com>
2015-09-10 22:45:45 -0400

    Audi Logo
    Hello Prof, sorry for late submission, please look over my work, thank you.

--- circles0.java
+++ circles0.java
@@ -2,10 +2,12 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(1080, 720);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
-}
+  ellipse( 720,350, 330,330 );       // Try changing the (x,y) and size.
+  ellipse( 320,350, 330,330 );
+  ellipse( 520,350, 330,330 );
+}                                    // Trying Audi logo, haha 
