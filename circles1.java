c6863c7a3e5d95a0618f65cfac1aabbe4032b601
Nayeem <exven19@gmail.com>
2015-09-10 23:02:28 -0400

    Ok
    Just messing with new ellipse or circle, that's all.

--- circles1.java
+++ circles1.java
@@ -5,17 +5,19 @@ float x,y;
 
 // Setup:  screen size, initialization. //
 void setup() {
-  size(500, 500);        // Try changing the width & height.
+  size( 1080, 720);        // Try changing the width & height.
   x=  width/2;
   y=  height/2;
 }
 
 // Next frame:  draw three circles. //
 void draw() {
-  ellipse(x, y, 80, 80);
-  ellipse(x+60, y+60, 40, 40);
-  ellipse(x+90, y+90, 20, 20);
- 
+  ellipse(x, y, 180, 180);
+  ellipse(x+160, y+160, 140, 140);
+  ellipse(x-160, y+160, 140, 140); // Added here new one
+  ellipse(x+290, y+290, 120, 120);
+  ellipse(x-290, y+290, 120, 120); // Another one
+  
   // Move them???? //--              x=  x + 2;  y=  y + 1;
   // Insert BREAK here ^
   // to uncomment the above code,
